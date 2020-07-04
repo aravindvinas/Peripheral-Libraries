a50a10e0a97f6fb0bd5011b8af0a805d6a61b2d7
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-07-04 14:11:24 +0530

    Update systick.h
    Update MAX_DELAY variable

--- Include/systick.h
+++ Include/systick.h
@@ -11,7 +11,7 @@
 
 #define tick_freq      1U
 
-// Systick Clock Source
+#define MAX_DELAY			 (0xFFFFFFFFU)
 #define SYSTICK_SOURCE_DIV8  (0x00000000U)
 #define SYSTICK_SOURCE       SysTick_CTRL_CLKSOURCE_Msk
 
@@ -22,13 +22,13 @@
 volatile uint32_t tick_count;
 
                                /***EXPORTED FUCNTIONS***/
-uint32_t get_tick(void)
+uint32_t get_tick(void);
 
-void time_inc(void)
+void time_inc(void);
 	
-uint32_t SysTick_Init()
+uint32_t SysTick_Init();
 
-void ms_delay(uint32_t period)
+void ms_delay(uint32_t period);
 
 void SysTick_ConfClkSrc(uint32_t ClkSrc);
 
