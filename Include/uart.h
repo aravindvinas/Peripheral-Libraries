b3bf1b57658297e8e191a1431ee14956541e6963
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-07-29 22:53:34 +0530

    Update uart.h

--- Include/uart.h
+++ Include/uart.h
@@ -16,16 +16,16 @@ typedef struct{
   uint32_t stop_bit;
   uint32_t mode;
   uint32_t hw_fctrl;
-}uart_config_typdef;
+}uart_configTypedef;
 
 typdef struct{
-  uart_config_typedef config;
+  uart_configTypedef config;
   USART_TypeDef reg;
   uint8_t *txBuf;
   uint8_t *rxBuf;
   uint32_t txState;
   uint32_t rxState; 
-}
+}uart_handleTypedef;
 
 //@ baud rate 
 #define USART_BAUD_9600   9600U
@@ -52,13 +52,13 @@ typdef struct{
 #define USART_Tx    2U
 #define USART_Tx_Rx 3U
 
-//@hardware flow control
+//@ hardware flow control
 #define USART_FCTRL_NONE    0U
 #define USART_FCTRL_RTS     1U
 #define USART_FCTRL_CTS     2U
 #define USART_FCTRL_RTS_CTS 3U
 
-uint32_t uart_init(uart_typedef *handle);
+uint32_t uart_init(uart_handleTypedef *handle);
 uint32_t uart_transmit(uint8_t *data);
 
 #endif
